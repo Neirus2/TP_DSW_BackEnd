03e14483b41bc358859fea91856ace3325277d1f
Manuel Martinez <mrm291201@gmail.com>
2023-10-12 10:51:01 -0300

    PRODUCTO TERMINADO v.2

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,6 +6,7 @@ exports.getProducts = async (req, res) => {
   try {
     const products = await Product.find();
     res.json(products);
+    console.log('HOLAAAAA')
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Error al obtener la lista de productos.' });
